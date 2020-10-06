f6b259b41d52b8f589a503703b07983d3b940b66
Juan Pablo <juanpablocs21@gmail.com>
2020-10-06 15:35:57 -0500

    feat: configure basic rules linter

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-let bar = 1;
-let foo = true;
+let bar  = 1;
+let foo  = true;
 let demo = "demo";
 
 if (bar) {
   console.log({ bar });
 }
+const o = { a:1 };
+console.log({ o });
+
+//demo
 if (foo) {
-    console.log("with err 4 spaces");
+  console.log("with err 4 spacess", demo);
 }
